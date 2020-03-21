9b0fd84115892e25949b6b13648856f1d5910b07
tomasz.zawadzki.eka <241503@student.pwr.edu.pl>
2020-03-21 17:03:16 +0100

    tozawadzki - Dodanie projektu WindowsForms, stworzenie połączenia z bazą danych (wersja lokalna, do skonfigurowania na własnej platformie) oraz stworzenie dwóch przykładowych tabel

---
+++ Aquapark/Aquapark/Aquapark.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Aquapark
+{
+    partial class Aquapark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customersGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.customersGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customersGridView
+            // 
+            this.customersGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.customersGridView.Location = new System.Drawing.Point(12, 12);
+            this.customersGridView.Name = "customersGridView";
+            this.customersGridView.Size = new System.Drawing.Size(299, 190);
+            this.customersGridView.TabIndex = 0;
+            // 
+            // Aquapark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1798, 669);
+            this.Controls.Add(this.customersGridView);
+            this.Name = "Aquapark";
+            this.Text = "Aquapark";
+            this.Load += new System.EventHandler(this.Aquapark_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.customersGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView customersGridView;
+    }
+}
+
---
+++ Aquapark/Aquapark/Aquapark.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Aquapark
+{
+    public partial class Aquapark : Form
+    {
+        public Aquapark()
+        {
+            InitializeComponent();
+        }
+
+        private void Aquapark_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            LoadCustomers();
+        }
+
+        private void LoadCustomers()
+        {
+            try
+            {
+                var connectionString = "Data Source=XE;User Id=Aquapark;Password=7zmty6q2e;";
+                using (var connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+                    var command = new OracleCommand("SELECT * FROM Aquapark.Customers", connection);
+                    var oracleDataAdapter = new OracleDataAdapter(command);
+                    var dataSet = new DataSet();
+                    oracleDataAdapter.Fill(dataSet);
+                    if (dataSet.Tables.Count > 0)
+                    {
+                        customersGridView.DataSource = dataSet.Tables[0].DefaultView;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
